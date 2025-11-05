9bef7d63877e816d683266abe6cb9db65b456401
Iker Dominguez Ajuria <iker.dominguez@opendeusto.es>
2025-11-05 16:03:40 +0100

    DB change

--- appVideomaniacos/models.py
+++ appVideomaniacos/models.py
@@ -1,28 +1,26 @@
 from django.db import models
 
 class Genero(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.TextField(blank=True)
+    nombre = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
 
 
 class Plataforma(models.Model):
-    nombre = models.CharField(max_length=50)
-    fabricante = models.CharField(max_length=50, blank=True)
+    nombre = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
 
 
 class Videojuego(models.Model):
-    titulo = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    fecha_lanzamiento = models.DateField()
+    titulo = models.CharField(max_length=200)  # 👈 cambio de 'nombre' a 'titulo'
+    descripcion = models.TextField(blank=True, null=True)
     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
     plataformas = models.ManyToManyField(Plataforma)
-    imagen = models.URLField(blank=True)
+    imagen = models.CharField(max_length=200, blank=True, null=True)
+    fecha_lanzamiento = models.DateField(blank=True, null=True)  # 👈 nuevo campo
 
     def __str__(self):
         return self.titulo
