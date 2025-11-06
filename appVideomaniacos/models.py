6c17e40eb170c1846b21f336c0abfabd49b97a8c
josuviteri <josu.viteri@opendeusto.es>
2025-11-06 13:31:56 +0100

    Add score field to Videojuego model and update imagen field

--- appVideomaniacos/models.py
+++ appVideomaniacos/models.py
@@ -21,6 +21,7 @@ class Videojuego(models.Model):
     plataformas = models.ManyToManyField(Plataforma)
     imagen = models.ImageField(upload_to='videojuegos/', blank=True, null=True)
     fecha_lanzamiento = models.DateField(blank=True, null=True)  # 👈 nuevo campo
+    score = models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return self.titulo
