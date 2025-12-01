13ba2d2e42c1ca908af6ff79f4b8590999e44e40
Iker Dominguez Ajuria <iker.dominguez@opendeusto.es>
2025-12-01 19:38:04 +0100

    get_list_or_404 implementado y fix menores

--- appVideomaniacos/views.py
+++ appVideomaniacos/views.py
@@ -1,14 +1,10 @@
-from pyexpat.errors import messages
+from django.contrib import messages
+from django.shortcuts import get_list_or_404
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+
 from .models import Videojuego, Genero, Plataforma, Contacto
 from .forms import ContactForm, VideojuegoForm
-
-
-from django.contrib import messages
-
-
-
 
 # 1. Vista de Inicio (Home)
 class IndexView(TemplateView):
@@ -16,33 +12,45 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Mantenemos tu lógica original: mostrar el último juego de cada género
+
+        # Mostrar el último juego publicado de cada género
         videojuegos_por_genero = {}
         for genero in Genero.objects.all():
-            juego = Videojuego.objects.filter(genero=genero).order_by('-fecha_lanzamiento').first()
+            juego = (
+                Videojuego.objects
+                .filter(genero=genero)
+                .order_by('-fecha_lanzamiento')
+                .first()
+            )
             if juego:
                 videojuegos_por_genero[genero] = juego
-        
+
         context['videojuegos_por_genero'] = videojuegos_por_genero
         return context
 
-# 2. Listados (Videojuegos, Géneros, Plataformas)
+# 2. Listados (videojuegos, géneros, plataformas)
 class VideojuegoListView(ListView):
-    model = Videojuego
     template_name = 'videojuegos.html'
-    context_object_name = 'videojuegos' # Así lo llamas en el HTML: {% for v in videojuegos %}
+    context_object_name = 'videojuegos'
+
+    def get_queryset(self):
+        return get_list_or_404(Videojuego)
 
 class GeneroListView(ListView):
-    model = Genero
     template_name = 'generos.html'
     context_object_name = 'generos'
 
+    def get_queryset(self):
+        return get_list_or_404(Genero)
+
 class PlataformaListView(ListView):
-    model = Plataforma
     template_name = 'plataformas.html'
     context_object_name = 'plataformas'
 
-# 3. Detalles (Ver un juego, ver un género, ver una plataforma)
+    def get_queryset(self):
+        return get_list_or_404(Plataforma)
+
+# 3. Detalles
 class VideojuegoDetailView(DetailView):
     model = Videojuego
     template_name = 'detalle_videojuego.html'
@@ -53,29 +61,17 @@ class GeneroDetailView(DetailView):
     template_name = 'detalle_genero.html'
     context_object_name = 'genero'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Pasamos los juegos de este género a la plantilla
-        context['videojuegos'] = self.object.videojuego_set.all()
-        return context
-
 class PlataformaDetailView(DetailView):
     model = Plataforma
     template_name = 'detalle_plataforma.html'
     context_object_name = 'plataforma'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Pasamos los juegos de esta plataforma a la plantilla
-        context['videojuegos'] = self.object.videojuego_set.all()
-        return context
-
-# 4. Formulario de Creación (Sustituye a tu antigua función 'sugerir_videojuego')
+# 4. Formularios
 class VideojuegoCreateView(CreateView):
     model = Videojuego
     form_class = VideojuegoForm
     template_name = 'formulario.html'
-    success_url = reverse_lazy('videojuegos') # Redirige aquí al terminar
+    success_url = reverse_lazy('videojuegos')
 
 class ContactoView(CreateView):
     model = Contacto
